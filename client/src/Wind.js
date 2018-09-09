d6b70042f499bc834911a111fa5d843f87bb0bd5
Viktorija <viktorija.bezsazna@gmail.com>
2018-09-09 15:41:35 -0400

    Add react-loadable and react-router-dom to enable frontend app splitting

---
+++ client/src/Wind.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+
+import './Wind.css';
+
+class Wind extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    console.log(body)
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">{this.state.response}</p>
+      </div>
+    );
+  }
+}
+
+export default Wind;
