44954efebdabc4929ff92a1aa321ef7e6beaf196
Viktorija <viktorija.bezsazna@gmail.com>
2018-09-14 21:10:57 -0400

    Add min amount of css

--- client/src/resultsPage/wind/WindCity.js
+++ client/src/resultsPage/wind/WindCity.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class WindCity extends Component {
   render() {
     return (
-      <div >
+      <div className="wind-cities">
         {this.props.windCityList.map(city => <div key={city.name} >{city.name}:{city.windSpeed}km/h</div>)}
       </div>
     );
