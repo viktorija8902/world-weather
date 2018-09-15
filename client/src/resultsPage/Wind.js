23ca8a27186ce523129d28286643e14022f9b300
Viktorija <viktorija.bezsazna@gmail.com>
2018-09-14 20:36:37 -0400

    Show cities grouped by wind speed

--- client/src/resultsPage/Wind.js
+++ client/src/resultsPage/Wind.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import WindSpeedSummary from "./WindSpeedSummary";
 import WindCity from "./WindCity";
+import WindCityGroups from './WindCityGroups';
 
 class Wind extends Component {
   render() {
     return (
-      <div >
+      <div>
         <WindCity windCityList={this.props.windData.windCityList}/>
-        {/* {citiesGroupedByWind} */}
+        <WindCityGroups citiesGroupedByWind={this.props.windData.citiesGroupedByWind}/>
         <WindSpeedSummary summary={this.props.windData.windSummary}/>
       </div>
     );
--- server/src/WindStatisticsGenerator.js
+++ server/src/WindStatisticsGenerator.js
@@ -5,13 +5,13 @@ export function generateWindData(citiesWeatherData) {
     const cityWindList = getCityWindList(sortedByWind);
 
     console.log("Winds in cities (km/h): ",  cityWindList)
-    const citiesGoupedByWind = getCitiesGroupedByWind(cityWindList);
-    console.log("citiesGoupedByWind", citiesGoupedByWind)
-    const windSummary = getWindSummary(citiesGoupedByWind, cityWindList)
+    const citiesGroupedByWind = getCitiesGroupedByWind(cityWindList);
+    console.log("citiesGroupedByWind", [...citiesGroupedByWind.entries()])
+    const windSummary = getWindSummary(citiesGroupedByWind, cityWindList)
     console.log("Wind statictics based on speed: ", windSummary)
     return {
         windCityList: cityWindList,
-        citiesGoupedByWind: citiesGoupedByWind,
+        citiesGroupedByWind: [...citiesGroupedByWind.entries()],
         windSummary: windSummary
     };
 }
@@ -36,14 +36,14 @@ const knotsToKmPerHour = (speed) => speed * 1.852;
 
 
 function getCitiesGroupedByWind(cityWindList) {
-    let citiesGoupedByWind = new Map();
+    let citiesGroupedByWind = new Map();
     
     let winds = Object.keys(WIND_SPEED);
     let speeds = Object.values(WIND_SPEED);
     let windsKmPerHour = new Map();
 
     for (let i = 0; i < winds.length; i++) {
-        citiesGoupedByWind.set(winds[i], []);
+        citiesGroupedByWind.set(winds[i], []);
         windsKmPerHour.set(winds[i], {
             min: knotsToKmPerHour(speeds[i].min),
             max: knotsToKmPerHour(speeds[i].max)
@@ -54,43 +54,43 @@ function getCitiesGroupedByWind(cityWindList) {
         const windSpeed = city.windSpeed;
         switch (true) {
             case (windSpeed < windsKmPerHour.get("LIGHT_WINDS").max):
-                addToWindMap(citiesGoupedByWind, "LIGHT_WINDS", city.name)
+                addToWindMap(citiesGroupedByWind, "LIGHT_WINDS", city.name)
                 break;
             case (windSpeed >= windsKmPerHour.get("MODERATE_WINDS").min && windSpeed <= windsKmPerHour.get("MODERATE_WINDS").max):
-                addToWindMap(citiesGoupedByWind, "MODERATE_WINDS", city.name)
+                addToWindMap(citiesGroupedByWind, "MODERATE_WINDS", city.name)
                 break;
             case (windSpeed >= windsKmPerHour.get("STRONG_WIND_SMALL_CRAFT_WARNING").min && windSpeed <= windsKmPerHour.get("STRONG_WIND_SMALL_CRAFT_WARNING").max):
-                addToWindMap(citiesGoupedByWind, "STRONG_WIND_SMALL_CRAFT_WARNING", city.name)   
+                addToWindMap(citiesGroupedByWind, "STRONG_WIND_SMALL_CRAFT_WARNING", city.name)   
                 break;
             case (windSpeed >= windsKmPerHour.get("GALE_WARNING").min && windSpeed <= windsKmPerHour.get("GALE_WARNING").max):
-                addToWindMap(citiesGoupedByWind, "GALE_WARNING", city.name) 
+                addToWindMap(citiesGroupedByWind, "GALE_WARNING", city.name) 
                 break;
             case (windSpeed >= windsKmPerHour.get("STORM_WARNING").min && windSpeed <= windsKmPerHour.get("STORM_WARNING").max):
-                addToWindMap(citiesGoupedByWind, "STORM_WARNING", city.name) 
+                addToWindMap(citiesGroupedByWind, "STORM_WARNING", city.name) 
                 break;
             case (windSpeed >= windsKmPerHour.get("HURRICANE_FORCE_WIND_WARNING").min):
-                addToWindMap(citiesGoupedByWind, "HURRICANE_FORCE_WIND_WARNING", city.name) 
+                addToWindMap(citiesGroupedByWind, "HURRICANE_FORCE_WIND_WARNING", city.name) 
                 break;
             default:
                 console.log("Error: ", city);
         }
     })
-    return citiesGoupedByWind;
+    return citiesGroupedByWind;
 }
 
-function getWindSummary(citiesGoupedByWind, cityWindList) {
+function getWindSummary(citiesGroupedByWind, cityWindList) {
     let windStatictics = {};
-    citiesGoupedByWind.forEach((value, key) => {
+    citiesGroupedByWind.forEach((value, key) => {
         windStatictics[key] = (value.length*100/cityWindList.length).toFixed(2)
     })
     return windStatictics;
 }
 
-function addToWindMap(citiesGoupedByWind, windType, cityName) {
-    const cityList = citiesGoupedByWind.get(windType)
+function addToWindMap(citiesGroupedByWind, windType, cityName) {
+    const cityList = citiesGroupedByWind.get(windType)
     if (cityList) {
-        citiesGoupedByWind.set(windType, cityList.concat(cityName))
+        citiesGroupedByWind.set(windType, cityList.concat(cityName))
     } else {
-        citiesGoupedByWind.set(windType, [cityName])
+        citiesGroupedByWind.set(windType, [cityName])
     }
 }
