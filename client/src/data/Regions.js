555834f54959e52a9e60c9ee029441367291a29f
Viktorija <viktorija.bezsazna@gmail.com>
2018-09-15 19:20:47 -0400

    Add more regions

--- client/src/data/Regions.js
+++ client/src/data/Regions.js
@@ -15,4 +15,23 @@ export const regionList = [
         { name: "europe west part", coord: { lonTopLeft: -13.280284, latTopRight: 71.233755, lonBottomRight: 16.828119, latBottomLeft: 34.366111 } },
       ]
     },
+    { main: {
+      name: "canada", coord: { lonTopLeft: -140.993778, latTopRight: 83.939953, lonBottomRight: -54.487557, latBottomLeft: 47.960502}
+      },
+      parts: [
+        { name: "canada east part", coord: { lonTopLeft: -97.026577, latTopRight: 60.737686, lonBottomRight: -53.430933, latBottomLeft: 48.443778 }},
+        { name: "canada west part", coord: { lonTopLeft: -140.993778, latTopRight: 83.189686, lonBottomRight: -96.677583, latBottomLeft: 47.960502 } },
+        { name: "southern ontario", coord: { lonTopLeft: -81.836645, latTopRight: 45.472651, lonBottomRight: -78.015075, latBottomLeft: 42.274260 } },
+      ]
+    },
+    { main: {
+        name: "new zealand", coord: { lonTopLeft: 165.005138, latTopRight: -32.219772, lonBottomRight: 185.386848, latBottomLeft:-53.256998 }
+      },
+      parts: []
+    },
+    { main: {
+      name: "australia", coord: { lonTopLeft: -252.373312, latTopRight: -10.935798, lonBottomRight: -204.933125, latBottomLeft: -43.782993 }
+    },
+    parts: []
+  }
 ];
