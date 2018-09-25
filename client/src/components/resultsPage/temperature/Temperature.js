6ffc083c47ccfbb102670051144bd6f036a84ceb
Viktorija <viktorija.bezsazna@gmail.com>
2018-09-25 19:49:58 -0400

    Fix temperature style

--- client/src/components/resultsPage/temperature/Temperature.js
+++ client/src/components/resultsPage/temperature/Temperature.js
@@ -3,13 +3,13 @@ import City from "../../generalComponents/City";
 
 class Temperature extends Component {
   render() {
-    const cityList = this.props.temperatureData.temperatureCityList.map(city => {
+    const temperatureCities = this.props.temperatureData.temperatureCityList.map(city => {
       return <City key={city.name} cityName={city.name} param={city.temperature} unit="&#8451;"/>
     });
     return (
       <div className="temperature-block">
         <h1>Temperature information</h1>
-        {cityList}
+        <div className="temperature-cities">{temperatureCities}</div>
       </div>
     );
   }
