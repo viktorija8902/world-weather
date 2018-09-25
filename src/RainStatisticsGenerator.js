794abb7ce91304512670dd19bea2da8b9ee35e30
Viktorija <viktorija.bezsazna@gmail.com>
2018-09-25 18:17:07 -0400

    Get rain data in different way, change the way it's shown

--- src/RainStatisticsGenerator.js
+++ src/RainStatisticsGenerator.js
@@ -1,35 +1,37 @@
 export function generateRainData(citiesWeatherData) {
-    const sortedByRain = sortByRain(citiesWeatherData);
-    const rainCityList = getCityRainList(sortedByRain);
-    const rainPercentage = getRainPercentage(rainCityList);
-
-    console.log("RainCityList: ", rainCityList)
-    console.log(`In the last 3 hours it was raining in ${rainPercentage} % of the selected cities`)
-    return {
-        rainCityList: rainCityList,
-        rainSummary: `In the last 3 hours it was raining in ${rainPercentage} % of the selected cities.` 
-    };
+  const rainIdsStartWith = 5;
+  const rainCityList = getCityRainList(rainIdsStartWith, citiesWeatherData)
+  rainCityList.sort((a,b) => a.rain != null && b.rain != null ? a.rain.localeCompare(b.rain) : a.rain == null ? 1 : -1);
+  const rainPercentage = getRainPercentage(rainCityList);
+  return {
+    rainCityList: rainCityList,
+    rainSummary: `It is raining in ${rainPercentage} % of the selected cities.`
+  };
 }
 
-function getCityRainList(data) {
-    return data.map(city => {
-        return {
-            name: city.name,
-            rainInLastThreeHours: city.rain
-        }
-    });
-}
-
-function sortByRain(citiesWeatherData) {
-    return citiesWeatherData.sort((a,b) => a.rain - b.rain);
+function getCityRainList(id, allCities) {
+  return allCities.map(city => {
+    const rain = city.weather.find(weather => weather.id.toString().startsWith(id));
+    if (rain) {
+      return {
+        name: city.name,
+        rain: rain.description
+      }
+    } else {
+      return {
+        name: city.name,
+        rain: null
+      }
+    }
+  });
 }
 
 function getRainPercentage(cities) {
-    const citiesWithRain = cities.filter(city => city.rainInLastThreeHours !== null)
-    return calculatePercentage(cities.length, citiesWithRain.length)
+  const citiesWithRain = cities.filter(city => city.rain !== null);
+  return calculatePercentage(cities.length, citiesWithRain.length);
 }
 
 //TODO move to separate file
 function calculatePercentage(numberOfCities, citiesWithSpecificWeather) {
-    return (citiesWithSpecificWeather*100/numberOfCities).toFixed(2);
+  return (citiesWithSpecificWeather * 100 / numberOfCities).toFixed(2);
 }
