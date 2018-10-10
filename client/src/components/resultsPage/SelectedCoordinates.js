0f09087c9f7d68a9fd472eb30bed5bf6d5b6d6b9
Viktorija <viktorija.bezsazna@gmail.com>
2018-10-10 17:28:38 -0400

    Style info about selected coordinates

--- client/src/components/resultsPage/SelectedCoordinates.js
+++ client/src/components/resultsPage/SelectedCoordinates.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import notSelected from './../../images/not-selected.png';
+import selected from './../../images/selected.png';
+
 
 function SelectedCoordinates(props) {
+  const notSelectedIcon = <img className="selection-icon" src={notSelected} alt="corner not selected"></img>
+  const selectedIcon = <img className="selection-icon" src={selected} alt="corner selected"></img>
   return (
-    <div className="selected-coords">
-      <div>Top left corner: {JSON.stringify(props.coordinates[0]) || "-" }</div>
-      <div>Top right corner: {JSON.stringify(props.coordinates[1]) || "-" }</div>
-      <div>Bottom right corner: {JSON.stringify(props.coordinates[2]) || "-" }</div>
-      <div>Bottom left corner: {JSON.stringify(props.coordinates[3]) || "-" }</div>
+    <div>
+      <div className="select-square-text">Select your weather square on the map!</div>
+      <div className="square-info">Top left corner: { props.coordinates[0] ? selectedIcon : notSelectedIcon }</div>
+      <div className="square-info">Top right corner: { props.coordinates[1] ? selectedIcon : notSelectedIcon }</div>
+      <div className="square-info">Bottom right corner: { props.coordinates[2] ? selectedIcon : notSelectedIcon }</div>
+      <div className="square-info">Bottom left corner: { props.coordinates[3] ? selectedIcon : notSelectedIcon }</div>
     </div>
   )
 }
 
-export default SelectedCoordinates
+export default SelectedCoordinates;
