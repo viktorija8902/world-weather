c3b6286e040182dc1b13c95253ef09b80a70ae05
Viktorija <viktorija.bezsazna@gmail.com>
2018-10-10 14:16:38 -0400

    Fix redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -11,8 +11,8 @@ import { App } from './App';
 
 const store = createStore(
     rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(thunk),
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
