cb9d890d75abeb656dffafe5c2b719c30e872ddb
Viktorija <viktorija.bezsazna@gmail.com>
2018-10-10 20:32:20 -0400

    Add footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div><a href="https://www.linkedin.com/in/viktorija-bezsazna/" target="_blank" rel="noopener noreferrer">About me</a></div>
+      <div>Thank you <a href="https://icons8.com" target="_blank" rel="noopener noreferrer">Icons8</a> for icons</div>
+    </div>
+  );
+}
+
+export default Footer;
