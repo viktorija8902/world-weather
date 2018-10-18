586e83d9b7e7243150bea94794037588ebc23c6b
Viktorija <viktorija.bezsazna@gmail.com>
2018-10-17 20:30:54 -0400

    Draw charts for temperature data

--- client/src/components/resultsPage/temperature/Temperature.js
+++ client/src/components/resultsPage/temperature/Temperature.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import City from "../../generalComponents/City";
+import Chart from "./../Chart";
+
+function sortBy(data, key) {
+  let arrayCopy = [...data];
+  arrayCopy.sort((a, b) => {
+    if (a[key] > b[key]) return -1;
+    if (a[key] < b[key]) return 1;
+    return 0;
+  });
+  return arrayCopy;
+}
 
 class Temperature extends Component {
   render() {
-    const temperatureCities = this.props.temperatureData.temperatureCityList.map(city => {
-      return <City key={city.id} cityName={city.name} param={city.temperature} unit="&#8451;"/>
-    });
+    let dataCopy = [...this.props.temperatureData.temperatureCityList];
+    const preparedForChart = dataCopy.map((city) => ({city: city.name, data: city.temperature }));
+    const sorted = sortBy(preparedForChart, "city");
+
     return (
       <div className="temperature-block">
-        <h1>Temperature information</h1>
-        <div className="temperature-cities">{temperatureCities}</div>
+        <h1>Temperature information (&#8451;)</h1>
+        <Chart cityWeather={sorted} unit="&#8451;"/>
       </div>
     );
   }
