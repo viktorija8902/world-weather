f7cf397aef78332511847f9ce6351911f58faba6
Viktorija <viktorija.bezsazna@gmail.com>
2018-10-20 11:37:31 -0400

    WordMap: Cities with rain/clouds are refreshed on the map when another region is selected

--- client/src/components/resultsPage/worldMap/WeatherButtons.js
+++ client/src/components/resultsPage/worldMap/WeatherButtons.js
@@ -5,25 +5,19 @@ import Button from "../../generalComponents/Button";
 class WeatherButtons extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      clickedButton: null,
-    }
     this.handleButtonClick = this.handleButtonClick.bind(this)
   }
 
   handleButtonClick(buttonId) {
-    this.setState({
-      clickedButton: buttonId,
-    })
     this.props.onWeatherButtonClick(buttonId);
   }
   
   render() {
     return (
       <div className="weather-buttons">
-        <Button id={WEATHER_BUTTON.RAIN} onButtonClick={this.handleButtonClick} name="Raining" isClicked={this.state.clickedButton === WEATHER_BUTTON.RAIN}/>
-        <Button id={WEATHER_BUTTON.CLOUD} onButtonClick={this.handleButtonClick} name="Cloudy" isClicked={this.state.clickedButton === WEATHER_BUTTON.CLOUD}/>
-        <Button id={WEATHER_BUTTON.RESET} onButtonClick={this.handleButtonClick} name="Reset" isClicked={this.state.clickedButton === WEATHER_BUTTON.RESET}/>
+        <Button id={WEATHER_BUTTON.RAIN} onButtonClick={this.handleButtonClick} name="Raining" isClicked={this.props.clickedButton === WEATHER_BUTTON.RAIN}/>
+        <Button id={WEATHER_BUTTON.CLOUD} onButtonClick={this.handleButtonClick} name="Cloudy" isClicked={this.props.clickedButton === WEATHER_BUTTON.CLOUD}/>
+        <Button id={WEATHER_BUTTON.RESET} onButtonClick={this.handleButtonClick} name="Reset" isClicked={this.props.clickedButton === WEATHER_BUTTON.RESET}/>
       </div>
     );
   }
