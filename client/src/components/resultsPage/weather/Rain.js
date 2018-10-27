32c7d4a079f45e24d550b6b57708ff401bf4a434
Viktorija <viktorija.bezsazna@gmail.com>
2018-10-27 16:04:22 -0400

    Refactor props, add 'Select country' note

--- client/src/components/resultsPage/weather/Rain.js
+++ client/src/components/resultsPage/weather/Rain.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import City from "../../generalComponents/City";
 
-const Rain = props => {
-  const rainCities = props.rainCities.map(city => 
+const Rain = ({rainCities}) => {
+  const cities = rainCities.map(city => 
     <City key={city.id} cityName={city.name} param={city.rain ? city.rain.description : "-"}/>
   );
   
   return (
     <div className="rain-block">
       <h1>Rain information</h1>
-      <div className="rain-cities">{rainCities}</div>
+      <div className="rain-cities">{cities}</div>
     </div>
   );
 }
