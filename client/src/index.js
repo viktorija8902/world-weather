e8f2933a0a53625d80a9e48b33332d1dd5c9cfea
Viktorija <viktorija.bezsazna@gmail.com>
2019-03-08 21:43:36 -0500

    Upgrade react

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { rootReducer } from './reducers';
@@ -9,10 +9,11 @@ import './index.scss';
 import { App } from './App';
 
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk),
+    composeEnhancer(applyMiddleware(thunk)),
 );
 
 ReactDOM.render(
