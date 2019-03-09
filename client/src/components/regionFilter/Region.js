585d0dbf3bbc093f96412a761eaf1415c4c074db
Viktorija <viktorija.bezsazna@gmail.com>
2019-03-08 21:56:55 -0500

    Region, RegionFilter containers uses hooks

--- client/src/components/regionFilter/Region.js
+++ client/src/components/regionFilter/Region.js
@@ -1,47 +1,38 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import RegionPart from "./RegionPart";
 
-class Region extends PureComponent {
-  state = {
-    clickedRegionPart: "",
-  };
+const Region = ({ onRegionClick, onPlaceSelection, region, isClicked }) => {
+  const [ clickedRegionPart, setRegionPart ] = useState("");
 
-  handleRegionClick = e => {
-    this.setState({
-      clickedRegionPart: "" 
-    });
-    this.props.onRegionClick(e.target.id)
+  const handleRegionClick = e => {
+    setRegionPart("");
+    onRegionClick(e.target.id)
   }
 
-  handleRegionPartClick = name => {
-    this.setState({
-      clickedRegionPart: name 
-    });
-    this.props.onPlaceSelection(name);
+  const  handleRegionPartClick = name => {
+    setRegionPart(name);
+    onPlaceSelection(name);
   }
 
-  render() {
-    const { region, isClicked } = this.props;
-    const subregions = region.parts.map(part => (
-      <RegionPart 
-        key={part.name}
-        regionPart={part}
-        onRegionPartClick={this.handleRegionPartClick}
-        isClicked={this.state.clickedRegionPart === part.name }/> 
-    ));
-    const cssSelectedClass = isClicked ? "selected-region" : "";
-    
-    return (
-      <div className="region-wrapper">
-        <div className={`region ${cssSelectedClass}`} id={region.main.name} onClick={this.handleRegionClick}>
-          {region.main.name.toUpperCase()}
-        </div>
-        {isClicked && <div>{subregions}</div>}
+  const subregions = region.parts.map(part => (
+    <RegionPart 
+      key={part.name}
+      regionPart={part}
+      onRegionPartClick={handleRegionPartClick}
+      isClicked={clickedRegionPart === part.name }/> 
+  ));
+  const cssSelectedClass = isClicked ? "selected-region" : "";
+  
+  return (
+    <div className="region-wrapper">
+      <div className={`region ${cssSelectedClass}`} id={region.main.name} onClick={handleRegionClick}>
+        {region.main.name.toUpperCase()}
       </div>
-    );
-  }
+      {isClicked && <div>{subregions}</div>}
+    </div>
+  );
 };
 Region.propTypes = {
   region: PropTypes.object.isRequired,
