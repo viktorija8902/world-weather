3d943781da8d8788038abf08de117303dad65ee1
Viktorija <viktorija.bezsazna@gmail.com>
2019-03-08 22:12:26 -0500

    Use hooks, effects in Wind.js

--- client/src/components/resultsPage/weather/Wind.js
+++ client/src/components/resultsPage/weather/Wind.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import City from "../../generalComponents/City";
 
-class Wind extends Component {
-  state = { 
-    highlightedCities: [],
-  }
-  colors = {
+const Wind = ({ clickedWindButton, windData }) => {
+  const [ highlightedCities, setCities ] = useState([]);
+
+  useEffect(() => {
+    setCities(getHighlightedCities(windData, clickedWindButton))
+  });
+
+  const colors = {
     LIGHT_WINDS: "#41d3f4",
     MODERATE_WINDS: "#37acc6",
     STRONG_WIND_SMALL_CRAFT_WARNING: "#e07841",
@@ -16,43 +19,31 @@ class Wind extends Component {
     HURRICANE_FORCE_WIND_WARNING: "#9241f4",
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps !== this.props) {
-      const { windData, clickedWindButton } = this.props;
-      this.setState({
-        highlightedCities: this.getHighlightedCities(windData, clickedWindButton),
-      });
-    }
-  }
-
-  getHighlightedCities(cities, windType) {
+  const getHighlightedCities = (cities, windType) => {
     return cities.filter(city => city.wind.type === windType);
   }
 
-  render() {
-    const { clickedWindButton, windData } = this.props;
-    const highlightColor = this.colors[clickedWindButton];
-    const windCities = windData.map(city => {
-      const isHighlighted = this.state.highlightedCities.find(c => c.id === city.id);
-      return <City 
-        key={city.id}
-        cityName={city.name}
-        param={city.wind.speed}
-        unit="km/h"
-        cssClass="wind-city"
-        specialStyle={{ backgroundColor: isHighlighted ? highlightColor : "" }}
-      />
-    })
-  
-    return (
-      <React.Fragment>
-        <h1>Wind information</h1>
-        <div className="wind-cities">
-          {windCities}
-        </div>
-      </React.Fragment>
-    );
-  }
+  const highlightColor = colors[clickedWindButton];
+  const windCities = windData.map(city => {
+    const isHighlighted = highlightedCities.find(c => c.id === city.id);
+    return <City 
+      key={city.id}
+      cityName={city.name}
+      param={city.wind.speed}
+      unit="km/h"
+      cssClass="wind-city"
+      specialStyle={{ backgroundColor: isHighlighted ? highlightColor : "" }}
+    />
+  })
+
+  return (
+    <React.Fragment>
+      <h1>Wind information</h1>
+      <div className="wind-cities">
+        {windCities}
+      </div>
+    </React.Fragment>
+  );
 }
 Wind.propTypes = {
   clickedWindButton: PropTypes.string.isRequired,
